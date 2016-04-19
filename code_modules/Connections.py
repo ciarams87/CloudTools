5b4fb30e844c4dec3d9818e6b5b6fbca95876050
ciarams87 <ciarastacke@hotmail.com>
2016-04-19 23:28:36 +0100

    Update Connections.py

--- code_modules/Connections.py
+++ code_modules/Connections.py
@@ -10,9 +10,12 @@ import boto.cloudtrail
 '''Define the access key id and secret access 
 key as set in the boto configuration file'''
 aws_access_key_id = config.get('Credentials', 'aws_access_key_id')
-#aws_access_key_id = ""
+# aws_access_key_id = ""
 aws_secret_access_key = config.get('Credentials', 'aws_secret_access_key')
-#aws_secret_access_key = ""
+# aws_secret_access_key = ""
+
+# noinspection PyMethodMayBeStatic
+
 
 class Connections:
 
@@ -21,26 +24,26 @@ class Connections:
         self.region = 'eu-west-1'
 
     def ec2_connection(self):
-        # Creates and return an EC2 Connection object
+        # Creates and returns an EC2 Connection object
         conn = boto.ec2.connect_to_region(self.region)
         return conn
 
     def s3_connection(self):
-        # Creates and return an S3 connection object
+        # Creates and returns an S3 connection object
         conn = S3Connection(aws_access_key_id, aws_secret_access_key)
         return conn
 
     def cw_connection(self):
-        # Creates and return a CloudWatch connection object
+        # Creates and returns a CloudWatch connection object
         conn = boto.ec2.cloudwatch.connect_to_region(self.region)
         return conn
 
     def as_connection(self):
-        # Creates and return an Auto Scaling connection object
+        # Creates and returns an Auto Scaling connection object
         conn = boto.ec2.autoscale.connect_to_region(self.region)
         return conn
 
     def ct_connection(self):
-        # Creates and return a CloudTrail connection object
+        # Creates and returns a CloudTrail connection object
         conn = boto.cloudtrail.connect_to_region(self.region)
         return conn
