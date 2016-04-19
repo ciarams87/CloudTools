e423c464d7ced35ac7eff3e8f1f65b6fd417ec09
ciarams87 <ciarastacke@hotmail.com>
2016-04-19 23:27:21 +0100

    Update aws_storage.py

--- code_modules/aws_storage.py
+++ code_modules/aws_storage.py
@@ -1,10 +1,12 @@
-# Class for programatically manipulating AWS's S3 service
+# Class for programmatically manipulating AWS's S3 service
 import os.path
+import boto
 
 
+# noinspection PyMethodMayBeStatic
 class aws_S3:
     def __init__(self):
-        ''' constructor '''
+        """constructor"""
 
     def list_buckets(self, conn):
         """ List all current AWS S3 buckets """
@@ -60,6 +62,7 @@ class aws_S3:
                 if key:
                     dl_file = path+"\\"+key.name
                     key.get_contents_to_filename(dl_file)
+                    print "File has been downloaded to %s" % dl_file
                 else:
                     print 'key %s cannot be found' % file_name
             else:
@@ -79,56 +82,65 @@ class aws_S3:
         else:
             print 'bucket cannot be found'
 
+    def new_bucket(self, conn, new_name):
+        try:
+            bucket = conn.create_bucket(new_name)
+            return bucket
+        except boto.exception.S3CreateError:
+            print new_name, 'cannot be created (name is not unique)'
+
 """
 Amazon Simple Storage Service (Amazon S3) is easy to use, secure, durable, 
-highly-scalable cloud object storage service. 
+highly-scalable cloud object storage service.
 
-line 10: gets list of buckets by calling get_all_buckets method on s3 connection
+line 12: gets list of buckets by calling get_all_buckets method on s3 connection
         object.
-lines 12/13: Loops through bucket list and prints name of each bucket to screen
+lines 14/15: Loops through bucket list and prints name of each bucket to screen
 
-line 17:if the path exists
-line 18:gets bucket by calling lookup method on s3 conn object using the name
+line 19:if the path exists
+line 20:gets bucket by calling lookup method on s3 conn object using the name
         of the bucket as parameter (lookup instead of get_bucket so exception is not thrown)
-line 19: if the bucket has been retrieved
-line 20: creates new key (file) object by calling new_key method on bucket object
+line 21: if the bucket has been retrieved
+line 22: creates new key (file) object by calling new_key method on bucket object
         and passing a name (string object) in as parameter
-line 21: sets the the contents of the newly created key object by calling the
+line 23: sets the the contents of the newly created key object by calling the
         set_contents_from_filename method on the key object and passing the
         source path of the contents as parameter
-lines 22-25: If the key contents have been set, prints success message to screen.
+lines 24-27: If the key contents have been set, prints success message to screen.
             If not, prints error message
-lines 26/27: If bucket cannot be retrieved, prints error message
-lines 28/29: if path does not exist, print error message
+lines 28/29: If bucket cannot be retrieved, prints error message
+lines 30/31: if path does not exist, print error message
 
-line 33: gets bucket by calling lookup method on s3 conn object using the name
+line 35: gets bucket by calling lookup method on s3 conn object using the name
         of the bucket as parameter
-line 34: if the bucket has been retrieved
-lines 35 - 38: gets list of files (keys) in the bucket by calling list method on bucket object.
+line 36: if the bucket has been retrieved
+lines 37 - 40: gets list of files (keys) in the bucket by calling list method on bucket object.
         Loops through file list and print the name of each file.
-lines 39/40: if bucket cannot be retrieved, prints error message
+lines 41/42: if bucket cannot be retrieved, prints error message
 
-lines 42-51: As above, but retrieves all buckets and loops through them
+lines 44-53: As above, but retrieves all buckets and loops through them
 
-line 55: if path exists
-line 56: gets bucket by calling lookup method on s3 conn object using the name
+line 57: if path exists
+line 58: gets bucket by calling lookup method on s3 conn object using the name
         of the bucket as parameter
-line 57: if the bucket has been retrieved
-line 58: retrieves key object using get_key method on bucket object and using the name of
+line 59: if the bucket has been retrieved
+line 60: retrieves key object using get_key method on bucket object and using the name of
         the key as parameter.
-lines 59: if key has been retrieved
-line 60: create filename to download key to by concatenating path, backslash and name of key
-line 61: calls get_contents_to_filename on the key object, passing in filename as parameter (method returns nothing)
-lines 62/63: if key cannot be retrieved, prints error message
-lines 64/65: if bucket cannot be retrieved, prints error message
-lines 66/67: if path not found, prints error message
+lines 61: if key has been retrieved
+line 62: create filename to download key to by concatenating path, backslash and name of key
+line 63: calls get_contents_to_filename on the key object, passing in filename as parameter (method returns nothing)
+lines 65/66: if key cannot be retrieved, prints error message
+lines 67/68: if bucket cannot be retrieved, prints error message
+lines 69/70: if path not found, prints error message
 
-line 71: gets bucket by calling lookup method on s3 conn object using the name
+line 74: gets bucket by calling lookup method on s3 conn object using the name
         of the bucket as parameter
-line 72: if the bucket has been retrieved
-line 73: deletes the key by calling delete_key method on bucket object and passing the key name
+line 75: if the bucket has been retrieved
+line 76: deletes the key by calling delete_key method on bucket object and passing the key name
         as parameter
-lines 74/75: If delete was successful, print success message, if not, print error message
-lines 76/77: If not, print error message
-lines 78/79: if bucket cannot be retrieved, prints error message
+lines 77/78: If delete was successful, print success message, if not, print error message
+lines 79/80: If not, print error message
+lines 81/82: if bucket cannot be retrieved, prints error message
+lines 85 - 89: Creates a new bucket of the specified name. Returns the bucket object if successful.
+                If it cannot be created (names must be unique), prints an error message.
 """
